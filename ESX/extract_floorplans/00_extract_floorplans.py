b856053698dc62e2b3b313af7895ac3d14af0a47
nickjvturner <nickjvturner@gmail.com>
2023-03-09 00:30:30 +0000

    built out per AP zoom script

---
+++ ESX/extract_floorplans/00_extract_floorplans.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+"""
+Created by Nick Turner (@nickjvturner)
+
+
+"""
+
+import zipfile
+import json
+import shutil
+import time
+from pathlib import Path
+from pprint import pprint
+
+
+def main():
+    nl = '\n'
+
+    # Get filename and current working directory
+    print(f'{nl}{Path(__file__).name}')
+    print(f'working_directory: {Path.cwd()}{nl}')
+
+    # Get local file with extension .esx
+    for file in sorted(Path.cwd().iterdir()):
+        # ignore files in directory containing _re-zip
+        if (file.suffix == '.esx') and (not('re-zip' in file.stem)):
+            proceed = input(f'Proceed with file: {str(file.name)}? (YES/no)')
+            if proceed == 'no':
+                exit()
+
+            print('filename:', file.name)
+
+            # Define the project name
+            project_name = file.stem
+            print('project_name:', project_name)
+
+            # Unzip the .esx project file into folder named {project_name}
+            with zipfile.ZipFile(file.name, 'r') as zip_ref:
+                zip_ref.extractall(project_name)
+            print('project successfully unzipped')
+
+            # Load the floorPlans.json file into the floorPlansJSON Dictionary
+            with open(Path(project_name) / 'floorPlans.json') as json_file:
+                floorPlans = json.load(json_file)
+            # pprint(floorPlans)
+
+            # Extract floorplans
+            for floor in floorPlans['floorPlans']:
+                # print(floor)
+                # print(Path(project_name) / floor['name'])
+                shutil.copy((Path(project_name) / ('image-' + floor['imageId'])), Path(floor['name']).with_suffix('.png'))
+
+            try:
+                shutil.rmtree(project_name)
+                print(f'Temporary project contents directory removed{nl}')
+            except Exception as e:
+                print(e)
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    run_time = time.time() - start_time
+    print(f'** Time to run: {round(run_time, 2)} seconds **')
