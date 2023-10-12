4daa5e8c1b12735a0789634e8c925022563eef54
nickjvturner <nickjvturner@gmail.com>
2023-10-12 13:46:26 +0100

    markup floorplans scripts

--- EKAHAU/markup_floorplans/01_markup_floorplans-SURVEYED.py
+++ EKAHAU/markup_floorplans/01_markup_floorplans-SURVEYED.py
@@ -14,13 +14,38 @@ import time
 import platform
 from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
-from pprint import pprint
-
 
 nl = '\n'
 
-# PIL Parameters
+# SUPER DUPER user-friendly variables for tweaking output
+#########################################################
+# AP icon
+# output size, this will depend on the resolution of the floor plans
+icon_resize = 200
+
+# TEXT
+font_size = 20
+
+# distance between AP icon centre point and top of text
+# this is probably somewhere around icon_resize / 4
+y_offsetter = 55
+
+# ROUNDED RECTANGLE
+# how much space between text and the edge of the rounded rectangle do you want
 offset = 5
+
+# add additional space before text
+text_pad_pre = 0
+
+# add additional space after text
+text_pad_post = 0
+
+rounded_rect_border_width = 2
+
+colourise_rounded_rect_border = False
+
+colourise_rounded_rect_fill = False
+
 
 def create_directory(new_dir):
     if not os.path.exists(new_dir):
@@ -33,9 +58,9 @@ def create_directory(new_dir):
 # Define text, font and size
 if platform.system() == "Windows":
     font_path = os.path.join(os.environ["SystemRoot"], "Fonts", "Consola.ttf")
-    font = ImageFont.truetype(font_path, 20)
+    font = ImageFont.truetype(font_path, font_size)
 else:
-    font = ImageFont.truetype("Menlo.ttc", 20)
+    font = ImageFont.truetype("Menlo.ttc", font_size)
 
 
 def text_width_and_height_getter(text):
@@ -61,7 +86,7 @@ def main():
     # Get local file with extension .esx
     for file in sorted(Path.cwd().iterdir()):
         # ignore files in directory containing _re-zip
-        if (file.suffix == '.esx') and (not('re-zip' in file.stem)):
+        if (file.suffix == '.esx') and (not ('re-zip' in file.stem)):
             proceed = input(f'Proceed with file: {str(file.name)}? (YES/no)')
             if proceed == 'no':
                 continue
@@ -88,48 +113,41 @@ def main():
             # Populate the intermediary dictionary with {floorId: floor name}
             for floor in floorPlans['floorPlans']:
                 floorPlansDict[floor['id']] = floor['name']
+
             # pprint(floorPlansDict)
 
             def floorPlanGetter(floorPlanId):
                 # print(floorPlansDict.get(floorPlanId))
                 return floorPlansDict.get(floorPlanId)
 
-            # Load the simulatedRadios.json file into the simulatedRadios dictionary
-            with open(Path(project_name) / 'simulatedRadios.json') as json_file:
-                simulatedRadios = json.load(json_file)
+            # Load the "surveyed" accessPoints.json file into the surveyedAPs dictionary
+            with open(Path(project_name) / 'accessPoints.json') as json_file:
+                surveyedAPs = json.load(json_file)
             # pprint(simulatedRadios)
 
-            # Create an intermediary dictionary to lookup simulated radio parameters
-            simulatedRadioDict = {}
+            # Create an intermediary dictionary to lookup surveyed radio parameters
+            surveyedAPsDict = {}
 
             # Populate the intermediary dictionary
-            for radio in simulatedRadios['simulatedRadios']:
-                simulatedRadioDict[radio['accessPointId']] = {}  # initialize nested dictionary
-                for x, y in radio.items():
-                    # print(x, y)
-                    simulatedRadioDict[radio['accessPointId']][x] = y
-
-            # pprint(simulatedRadioDict)
-            # print(simulatedRadioDict['84d58cfc-1121-4b8c-97e7-c589f56494e4']['antennaHeight'])
-
-            # Load the accessPoints.json file into the accessPoints dictionary
-            with open(Path(project_name) / 'accessPoints.json') as json_file:
-                accessPoints = json.load(json_file)
-            # pprint(accessPoints)
+            for surveyedAP in surveyedAPs["accessPoints"]:
+                if "color" in surveyedAP:
+                    surveyedAPsDict[surveyedAP["id"]] = {}  # initialize nested dictionary
+                    surveyedAPsDict[surveyedAP["id"]]["name"] = surveyedAP["name"]
+                    surveyedAPsDict[surveyedAP["id"]]["color"] = surveyedAP["color"]
+                    surveyedAPsDict[surveyedAP["id"]]["floorPlanId"] = surveyedAP["location"]["floorPlanId"]
+                    surveyedAPsDict[surveyedAP["id"]]["userDefinedPosition"] = surveyedAP["userDefinedPosition"]
+                    for x, y in surveyedAP["location"]["coord"].items():
+                        surveyedAPsDict[surveyedAP['id']][x] = y
 
             # Create directory to hold output directories
             create_directory(Path.cwd() / 'OUTPUT')
-
-            # Create subdirectory for Blank floor plans
-            plain_floorplan_destination = Path.cwd() / 'OUTPUT' / 'blank'
-            create_directory(plain_floorplan_destination)
 
             # Create subdirectory for Annotated floorplans
             annotated_floorplan_destination = Path.cwd() / 'OUTPUT' / 'annotated'
             create_directory(annotated_floorplan_destination)
 
             # Import custom icons
-            spot = Image.open(Path.cwd().parent / 'assets' / 'XO.png')
+            spot = Image.open(Path.cwd().parent / 'assets' / 'spot.png')
 
             # Resize icons if necessary (default size is 350x350 px)
             icon_resize = 200
@@ -139,12 +157,8 @@ def main():
             # Define the centre point of the icon
             spot_centre_point = (icon_resize // 2, icon_resize // 2)
 
-            # Offset the text below the AP icon
-            y_offsetter = spot.height / 4
-
             for floor in floorPlans['floorPlans']:
                 # Extract floorplans
-                shutil.copy((Path(project_name) / ('image-' + floor['imageId'])), Path(plain_floorplan_destination / floor['name']).with_suffix('.png'))
                 shutil.copy((Path(project_name) / ('image-' + floor['imageId'])), floor['imageId'])
 
                 # Open the floorplan to be used for all AP placement
@@ -153,18 +167,15 @@ def main():
                 # Create an ImageDraw object
                 draw_all_APs = ImageDraw.Draw(all_APs)
 
-                for ap in sorted(accessPoints['accessPoints'], key=lambda i: i['name']):
-                    # print(ap)
-                    # print(ap['location']['floorPlanId'])
+                for ap in surveyedAPsDict:
 
-                    if ap['location']['floorPlanId'] == floor['id']:
-
+                    if surveyedAPsDict.get(ap).get("floorPlanId") == floor['id']:
                         # establish x and y
-                        x, y = (ap['location']['coord']['x'], ap['location']['coord']['y'])
+                        x, y = (surveyedAPsDict.get(ap).get("x"), surveyedAPsDict.get(ap).get("y"))
 
                         print(
-                            f"{nl}[[ {ap['name']} [{ap['model']}]] from: {floorPlanGetter(ap['location']['floorPlanId'])} ] "
-                            f"has coordinates {x}, {y}")
+                            f'{nl}[[ {surveyedAPsDict.get(ap).get("name")} ]] from: {floorPlanGetter(surveyedAPsDict.get(ap).get("floorPlanId"))} ]'
+                            f'has coordinates {x}, {y}')
 
                         # Calculate the top-left corner of the icon based on the center point and x, y
                         top_left = (int(x) - spot_centre_point[0], int(y) - spot_centre_point[1])
@@ -173,20 +184,33 @@ def main():
                         all_APs.paste(spot, top_left, mask=spot)
 
                         # Handle AP Names drawn onto overview maps
-                        ap_name = ap['name']
+                        ap_name = surveyedAPsDict.get(ap).get("name")
 
                         # Calculate the height and width of the text
                         text_width, text_height = text_width_and_height_getter(ap_name)
 
                         # Establish coordinates for the rounded rectangle
-                        x1 = x - (text_width / 2) - offset
+                        x1 = x - (text_width / 2) - offset - text_pad_pre
                         y1 = y + y_offsetter - (text_height / 2) - offset
-                        x2 = x + (text_width / 2) + offset
+                        x2 = x + (text_width / 2) + offset + text_pad_post
                         y2 = y + y_offsetter + (text_height / 2) + offset
                         r = (y2 - y1) / 3
 
+                        # define colour of rounded rectangle border
+                        if not colourise_rounded_rect_border:
+                            rounded_rect_border_color = "black"
+                        else:
+                            rounded_rect_border_color = surveyedAPsDict.get(ap).get("color")
+
+                        # define colour of rounded rectangle fill
+                        if not colourise_rounded_rect_fill:
+                            rounded_rect_fill_color = "white"
+                        else:
+                            rounded_rect_fill_color = surveyedAPsDict.get(ap).get("color")
+
                         # draw the rounded rectangle
-                        draw_all_APs.rounded_rectangle((x1, y1, x2, y2), r, fill='white', outline='black', width=2)
+                        draw_all_APs.rounded_rectangle((x1, y1, x2, y2), r, fill=rounded_rect_fill_color,
+                                                       outline=rounded_rect_border_color, width=rounded_rect_border_width)
 
                         # draw the text
                         draw_all_APs.text((x + 1, y + y_offsetter + 1), ap_name, anchor='mm', fill='black', font=font)
