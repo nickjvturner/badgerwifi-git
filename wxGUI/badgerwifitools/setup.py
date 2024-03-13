f93f6d02a5b52db29899fb27ec2ae2a41087c902
nickjvturner <nickjvturner@gmail.com>
2024-03-13 16:15:55 +0100

    faulty ap-bracket logic found by Kenneth. broken logic fixed, project_IK AP List should now contain ap-bracket tag value.

--- wxGUI/badgerwifitools/setup.py
+++ wxGUI/badgerwifitools/setup.py
@@ -22,9 +22,8 @@ setup(
         'pillow>=10.2.0'
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: MIT License',  # Specify your project's license
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
@@ -34,7 +33,7 @@ setup(
     python_requires='>=3.9',
     entry_points={
         'console_scripts': [
-            'badgerwifi-tools=your_package_name.your_main_script:main_function',
+            'badgerwifitools=badgerwifitools.main:main',
         ],
     },
 )
