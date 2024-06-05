79a4d4b652954f30234d5d58f7d08fa10a86d085
nickjvturner <nickjvturner@gmail.com>
2024-06-05 17:24:58 +0100

    validate formatting reworked persistent failure fixed added model to antenna mount & tilt test

--- wxGUI/badgerwifitools/esx_actions/validate_esx.py
+++ wxGUI/badgerwifitools/esx_actions/validate_esx.py
@@ -11,67 +11,77 @@ from common import acceptable_antenna_tilt_angles
 from common import FIVE_GHZ_RADIO_ID
 
 nl = '\n'
+SPACER = '\n\n\n'
+PASS = '----\nPASS\n----\n'
+FAIL = '----\nFAIL\n----\n'
+CAUTION = '-------\nCAUTION\n-------\n'
+HASH_BAR = '\n\n#########################\n\n'
 
 
 def validate_color_assignment(offenders, total_ap_count, message_callback):
+    message_callback(f"{SPACER}### COLOUR ASSIGNMENT ###")
     if len(offenders.get('color', [])) > 0:
-        message_callback(f"{nl}Colour assignment test FAILED{nl}The following {len(offenders.get('color', []))} APs have been assigned no color")
+        message_callback(f"{FAIL}The following {len(offenders.get('color', []))} APs have been assigned no color")
         for ap in offenders['color']:
             message_callback(ap)
         return False
-    message_callback(f"{nl}Colour assignment test: PASSED{nl}All {total_ap_count} APs have a non-default colour")
+    message_callback(f"{PASS}All {total_ap_count} APs have a non-default colour{nl}")
     return True
 
 
 def validate_height_manipulation(offenders, total_ap_count, message_callback):
+    message_callback(f"{SPACER}### ANTENNA HEIGHT ###")
     if len(offenders.get('antennaHeight', [])) > 0:
-        message_callback(f"{nl}Caution! The following {len(offenders.get('antennaHeight', []))} APs are configured with the Ekahau 'default' height of 2.4 meters, is this intentional?")
+        message_callback(f"{CAUTION}The following {len(offenders.get('antennaHeight', []))} APs are configured with the Ekahau 'default' height of 2.4 meters, is this intentional?")
         for ap in offenders['antennaHeight']:
             message_callback(ap)
         return True
-    message_callback(f"{nl}antennaHeight manipulation test: PASSED{nl}All {total_ap_count} APs have an assigned height other than '2.4' metres")
+    message_callback(f"{PASS}All {total_ap_count} APs have an assigned height other than '2.4' metres")
     return True
 
 
 def validate_required_tags(offenders, total_ap_count, total_required_tag_keys_count, required_tag_keys, message_callback):
+    message_callback(f"{SPACER}### REQUIRED TAGS ###")
     # Initialize a list to store failed tag validations
     pass_required_tag_validation = []
 
     for missing_tag in offenders['missing_required_tags']:
         if len(offenders.get('missing_required_tags', {}).get(missing_tag, [])) > 0:
             message_callback(
-                f"{nl}There is a problem! The following {len(offenders.get('missing_required_tags', {}).get(missing_tag, []))} APs are missing the '{missing_tag}' tag")
+                f"{FAIL}There is a problem! The following {len(offenders.get('missing_required_tags', {}).get(missing_tag, []))} APs are missing the '{missing_tag}' tag")
             for ap in sorted(offenders['missing_required_tags'][missing_tag]):
                 message_callback(ap)
             pass_required_tag_validation.append(False)
         pass_required_tag_validation.append(True)
 
     if all(pass_required_tag_validation):
-        message_callback(f"{nl}{total_required_tag_keys_count} Required tag keys defined{nl}Required tag keys assignment test: PASSED")
+        message_callback(f"{PASS}{total_required_tag_keys_count} tag keys are defined:")
         for tagKey in required_tag_keys:
             message_callback(f"{tagKey}")
-        message_callback(f"All {total_ap_count} APs have the required {total_required_tag_keys_count} tag keys assigned:")
+        message_callback(f"All {total_ap_count} APs have the required {total_required_tag_keys_count} tag keys assigned")
         return True
     return False
 
 
 def validate_antenna_tilt(offenders, total_ap_count, message_callback):
+    message_callback(f"{SPACER}### ANTENNA TILT ###")
     if len(offenders.get('antennaTilt', [])) > 0:
-        message_callback(f"{nl}Antenna Tilt test: FAILED!{nl}The following {len(offenders.get('antennaTilt', []))} APs have an antenna tilt that will cause problems when generating per AP installer documentation")
+        message_callback(f"{FAIL}The following {len(offenders.get('antennaTilt', []))} APs have an antenna tilt that will cause problems when generating per AP installer documentation")
         for ap in offenders['antennaTilt']:
             message_callback(ap)
         return False
-    message_callback(f"{nl}Antenna Tilt test: PASSED{nl}All {total_ap_count} APs have a conforming antenna tilt")
+    message_callback(f"{PASS}All {total_ap_count} APs have an antenna tilt value that will work with the per AP installer documentation generation process{nl}")
     return True
 
 
-def validate_antenna_mounting_and_tilt_mismatch(offenders, total_ap_count, message_callback):
+def validate_antenna_mounting_and_tilt_mismatch(offenders, total_ap_count, message_callback, custom_ap_dict):
+    message_callback(f"{SPACER}### ANTENNA MOUNTING AND TILT ###")
     if len(offenders.get('antennaMounting_and_antennaTilt_mismatch', [])) > 0:
-        message_callback(f"{nl}Caution! The following {len(offenders.get('antennaMounting_and_antennaTilt_mismatch', []))} APs may be configured incorrectly{nl}These APs are WALL mounted with 0 degrees of tilt, is this intentional?")
+        message_callback(f"{CAUTION}The following {len(offenders.get('antennaMounting_and_antennaTilt_mismatch', []))} APs may be configured incorrectly{nl}These APs are WALL mounted with 0 degrees of tilt, is this intentional?")
         for ap in offenders['antennaMounting_and_antennaTilt_mismatch']:
-            message_callback(ap)
+            message_callback(f"{ap} | {custom_ap_dict[ap]['model']}")
         return True
-    message_callback(f"{nl}Antenna Mounting and Tilt Mismatch test: PASSED{nl}All {total_ap_count} APs have a conforming antenna mounting and tilt")
+    message_callback(f"{PASS}All {total_ap_count} APs have a conforming antenna mounting and tilt")
     return True
 
 
@@ -83,9 +93,11 @@ def validate_view_as_mobile_disabled(project_configuration_json, message_callbac
             break
 
     if view_as_mobile == "true":
-        bar = f"{nl}{nl}{'#' * 50}{nl}{nl}"
-        message_callback(f"{bar}View as mobile is enabled, this is a DISASTER{bar}")
+        message_callback(f"{SPACER}### VIEW AS MOBILE ###")
+        message_callback(f"{FAIL}View as mobile is enabled, this is a DISASTER")
         return False
+    return True
+
 
 def validate_esx(working_directory, project_name, message_callback, required_tag_keys, optional_tag_keys):
     message_callback(f'Performing Validation for: {project_name}')
@@ -145,13 +157,13 @@ def validate_esx(working_directory, project_name, message_callback, required_tag
         validate_height_manipulation(offenders, total_ap_count, message_callback),
         validate_required_tags(offenders, total_ap_count, total_required_tag_keys_count, required_tag_keys, message_callback),
         validate_antenna_tilt(offenders, total_ap_count, message_callback),
-        validate_antenna_mounting_and_tilt_mismatch(offenders, total_ap_count, message_callback),
+        validate_antenna_mounting_and_tilt_mismatch(offenders, total_ap_count, message_callback, custom_ap_dict),
         validate_view_as_mobile_disabled(project_configuration_json, message_callback),
     ]
 
     # Print pass/fail states
     if all(validations):
-        message_callback(f"{nl}### Validation PASSED! ###")
+        message_callback(f"{HASH_BAR}### VALIDATION PASSED ###{HASH_BAR}")
     else:
-        message_callback(f"{nl}### Validation FAILED ###")
+        message_callback(f"{HASH_BAR}### VALIDATION FAILED ###{HASH_BAR}")
     return
