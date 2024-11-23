75e07bb2482b22d06fffd4a8d4b029bc7a21bd15
nickjvturner <nickjvturner@gmail.com>
2024-11-23 06:19:44 +0000

    handle multiple image notes and multiple note images

--- wxGUI/badgerwifitools/exports/export_ap_images.py
+++ wxGUI/badgerwifitools/exports/export_ap_images.py
@@ -3,15 +3,8 @@
 """
 The original, well written script by Francois Verges (@VergesFrancois)
 Adapted, modified, mangled by Nick Turner (@nickjvturner)
-
-This script will extract all image notes attached to an AP objects of an
-Ekahau project file (.esx)
-
-It will place ALL the pictures into a single, new directory called AP_images
-
-Script should still work if you have multiple pictures per AP note
-Working as of 01-FEB-2023
 """
+import wx
 
 from common import load_json
 from common import nl
@@ -35,10 +28,10 @@ def export_ap_images(working_directory, project_name, message_callback):
 	notes_json = load_json(project_dir, 'notes.json', message_callback)
 
 	if not notes_json:
-		message_callback(f'No notes found in the project{nl}')
+		wx.CallAfter(message_callback, f'No notes found in the project{nl}')
 		return
 
-	message_callback(f'Extracting AP Images from: {project_name}')
+	wx.CallAfter(message_callback, f'Extracting AP Images from: {project_name}{nl}')
 
 	image_extraction_counter = []
 
@@ -52,12 +45,15 @@ def export_ap_images(working_directory, project_name, message_callback):
 			# Check if the AP is placed on a map
 			if 'location' in ap.keys():
 
+				# Determine if there are multiple notes with images for this AP
+				image_notes_count = sum(1 for ap_note in ap['noteIds'] for note in notes_json['notes'] if note['id'] == ap_note and len(note['imageIds']) > 0)
+
 				for ap_note in ap['noteIds']:
 
 					# Loop through all the notes stored within the project
 					for note in notes_json['notes']:
 
-						# Filter out notes that do not contain images
+						# Skip notes that do not contain images
 						if len(note['imageIds']) > 0:
 
 							# Check if the note id matches the current AP note id
@@ -65,25 +61,25 @@ def export_ap_images(working_directory, project_name, message_callback):
 
 								# Loop through all the images attached to the note
 								for image in note['imageIds']:
-
 									source_image_file = 'image-' + image
 									source_image_full_path = project_dir / source_image_file
 
-									# Check if there is more than 1 note containing an image
-									if len(note['imageIds']) > 1:
-										# there must be more than 1 image, we should add '-1', '-2', '-3', etc
-										ap_image_name = f"{ap['name']}-{str(image_count)}.png"
+									# Determine the output image name
+									if image_notes_count > 1 or len(note['imageIds']) > 1:
+										# Add image count starting from 1 if there are multiple images associated with this AP
+										ap_image_name = f"{ap['name']}-{image_count}.png"
 									else:
+										# Only one note with images, so no suffix for the first image
 										ap_image_name = f"{ap['name']}.png"
 
 									output_destination = ap_images_dir / ap_image_name
 
-									# count total number of APs extracted
+									# Count total number of APs extracted
 									image_extraction_counter.append(source_image_file)
 
 									shutil.copy(source_image_full_path, output_destination)
-									message_callback(f"{ap['name']} Image extracted")
+									wx.CallAfter(message_callback, f"{ap_image_name} Image extracted")
 
 									image_count += 1
 
-	message_callback(f'{nl}{len(image_extraction_counter)} images extracted{nl}')
+	wx.CallAfter(message_callback, f'{nl}{len(image_extraction_counter)} images extracted{nl}')
